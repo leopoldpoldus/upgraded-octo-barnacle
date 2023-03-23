a0459f40f537708aa3ca3f31f40969efc7583fa2
Leopold Wieser <leopoldwieser1@gmail.com>
2023-03-23 01:06:30 +0100

    added chat GPT api

---
+++ app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+
+def set_bg_hack_url():
+    '''
+    A function to unpack an image from url and set as bg.
+    Returns
+    -------
+    The background.
+    '''
+
+    st.markdown(
+        f"""
+         <style>
+         .stApp {{
+             background: url("https://cdn.pixabay.com/photo/2020/06/19/22/33/wormhole-5319067_960_720.jpg");
+             background-size: cover
+         }}
+         </style>
+         """,
+        unsafe_allow_html=True
+    )
+
+
+set_bg_hack_url()
+# Add your Streamlit app code here
